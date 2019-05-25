96ca01d5e908e84e729667b654b73caed38db10a
Jeff Hill <jeff@reverentengineer.com>
2019-05-25 09:59:14 -0400

    Cleanup install.js

--- routes/install.js
+++ routes/install.js
@@ -1,13 +1,7 @@
-const exec = require('child_process').exec;
-const fs = require('fs')
-const config = require('../models/config');
 var express = require('express');
-var async = require('async');
 var router = express.Router();
-var initializeDb = require('../models/db').initialize;
 var password = require('../models/password');
 
-// Redirect if the database is already installed
 router.use(function (req, res, next) {
     const db = req.app.locals.db;
     db.User.count({})
